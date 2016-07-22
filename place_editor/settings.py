5d288d4ddd3d57d04e3a43980feb543f805ebe87
Finlay McCourt <finlaymccourt@gmail.com>
2016-07-22 16:23:58 +0100

    Bit of initial cleanup.

--- place_editor/settings.py
+++ place_editor/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'place_editor.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'place_editor_db.sqlite3'),
     }
 }
 
